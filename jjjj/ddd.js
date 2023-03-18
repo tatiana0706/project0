043dae0a45f4db943c35450d72be41cb68ff5265
tatiana0706 <128232834+tatiana0706@users.noreply.github.com>
2023-03-18 19:53:59 +0600

    JS
    Добавлены задания js

---
+++ jjjj/ddd.js
@@ -0,0 +1,53 @@
+let test=17;
+if(test<=10){
+    document.write('true')
+}
+
+else{
+    document.write('false')
+}
+let tes=17;
+if(tes>=10){
+    document.write('true')
+}
+else{
+    document.write('false')
+}
+
+
+let test1=11;
+let test2=10;
+if(test1>test2){
+    document.write('false')
+}
+else{
+    document.write('true')
+}
+let tes1=11;
+let tes2=10;
+if(tes1=tes2){
+    document.write('false')
+}
+else{
+    document.write('true')
+}
+
+let num=3;
+let z=0;
+let x=5;
+if(z<num<x){
+    document.write('true')
+}
+else{
+    document.write('false')
+}
+
+let m=3;
+let c=0;
+let v=5;
+if(c<m<v){
+    document.write('true')
+}
+else{
+    document.write('false')
+}
---
+++ jjjj/main.js
@@ -0,0 +1,26 @@
+let name="Динара"
+let age=33 ;
+let n=3844;
+let planet="Марс"
+let form="Прямоугольная"
+console.log (typeof(name));
+console.log(typeof(age));
+console.log(typeof(n));
+console.log(typeof(planet));
+console.log(typeof(form));
+
+
+let nn=6789n;
+let t=true;
+let f= "foo";
+console.log(typeof(nn));
+console.log(typeof(true));
+console.log(typeof(f))
+
+let second=60;
+let day=24;
+console.log(second*day);
+
+let year=365;
+let secon=24;
+console.log(year*secon)
